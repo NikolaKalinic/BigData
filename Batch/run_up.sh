0c972d49b0194dae7a3687cfe13fc422de01c046
Nikola <nikolaa.kalinic@gmail.com>
2024-01-23 15:11:13 +0100

    update script and add 5 queries for batch

--- Batch/run_up.sh
+++ Batch/run_up.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function display_loading_animation() {
+    local duration=$1  # Number of seconds for the loading animation
+    local interval=$2   # Interval between characters
+
+    for ((i=0; i<duration; i++)); do
+        printf "#"
+        sleep "$interval"
+    done
+
+    echo ""
+}
+
 echo ">>>> Waiting for docker-compose..." 
 docker-compose up -d > logs/compose_output.log 2>&1
 compose_exit_code=$?
@@ -8,9 +20,8 @@ if [ $compose_exit_code -ne 0 ]; then
 	status="UNSUCCESSFULLY"
 fi
 echo ">>>> Docker-compose complited $status !"
-echo ">>>> ###################################"
+display_loading_animation 40 0.4
 echo ">>>> Waiting to transfering file to hdfs..."
-sleep 10 
 docker exec namenode hdfs dfs -copyFromLocal NoviSad.csv / > logs/transfer_output.log 2>&1
 docker exec namenode hdfs dfs -copyFromLocal Belgrade.csv / >> logs/transfer_output.log 2>&1
 docker exec namenode hdfs dfs -copyFromLocal Nis.csv / >> logs/transfer_output.log 2>&1
@@ -22,9 +33,8 @@ if [ $transfer_exit_code -ne 0 ]; then
 	status="UNSUCCESSFULLY"
 fi
 echo ">>>> Transfering file to hdfs complited $status !"
-echo ">>>> ###################################"
+display_loading_animation 40 0.2
 echo ">>>> Waiting preprocesing with spark..."
-sleep 5
 docker exec spark-master ./spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.10 ./preprocessing.py > logs/spark_output.log 2>&1
 spark_exit_code=$?
 status="SUCCESSFULLY"
@@ -32,7 +42,36 @@ if [ $spark_exit_code -ne 0 ]; then
 	status="UNSUCCESSFULLY"
 fi
 echo ">>>> Preprocesing complited $status !"
-echo ">>>> ###################################"
+display_loading_animation 40 0.03
+echo ">>>> Waiting winter with spark..."
+docker exec spark-master ./spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.10 ./winter.py > logs/spark_output.log 2>&1
+winter_exit_code=$?
+status="SUCCESSFULLY"
+if [ $winter_exit_code -ne 0 ]; then
+	status="UNSUCCESSFULLY"
+fi
+echo ">>>> Winter complited $status !"
+display_loading_animation 40 0.03
+echo ">>>> Waiting wind with spark..."
+docker exec spark-master ./spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.10 ./wind.py > logs/spark_output.log 2>&1
+wind_exit_code=$?
+status="SUCCESSFULLY"
+if [ $wind_exit_code -ne 0 ]; then
+	status="UNSUCCESSFULLY"
+fi
+echo ">>>> Wind complited $status !"
+display_loading_animation 40 0.03
+echo ">>>> Waiting season with spark..."
+docker exec spark-master ./spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.10 ./season.py > logs/spark_output.log 2>&1
+season_exit_code=$?
+status="SUCCESSFULLY"
+if [ $season_exit_code -ne 0 ]; then
+	status="UNSUCCESSFULLY"
+fi
+echo ">>>> Season complited $status !"
+display_loading_animation 40 0.03
+
+
 
 #hive://hive@hive-server:10000/default
 
--- Real-Time/run_up.sh
+++ Real-Time/run_up.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function display_loading_animation() {
+    local duration=$1  # Number of seconds for the loading animation
+    local interval=$2   # Interval between characters
+
+    for ((i=0; i<duration; i++)); do
+        printf "#"
+        sleep "$interval"
+    done
+
+    echo ""
+}
+
 echo ">>>> Waiting for docker-compose..." 
 docker-compose up -d > logs/compose_output.log 2>&1
 compose_exit_code=$?
@@ -8,9 +20,8 @@ if [ $compose_exit_code -ne 0 ]; then
 	status="UNSUCCESSFULLY"
 fi
 echo ">>>> Docker-compose complited $status !"
-echo ">>>> ###################################"
+display_loading_animation 40 0.2
 echo ">>>> Waiting to starting spark stream..."
-sleep 5
 docker exec spark-master-rt ./spark/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 ./consumer.py > logs/spark_output.log 2>&1 &
 stream_exit_code=$?
 status="SUCCESSFULLY"
@@ -18,11 +29,3 @@ if [ $stream_exit_code -ne 0 ]; then
 	status="UNSUCCESSFULLY"
 fi
 echo ">>>> Starting spark stream complited $status !"
-echo ">>>> ###################################"
-# docker exec kafka kafka-topics --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 2 --topic raw
-
-# docker exec -i producer bash -c "echo '* * * * * /usr/bin/python3 /produce_event.py' | crontab - && service cron start"
-
-# docker exec -t consumer bash -c "python3 consumer.py"
-
-#docker exec spark-master-rt ./spark/bin/spark-submit --packages org.postgresql:postgresql:42.2.10 ./consumer.py
